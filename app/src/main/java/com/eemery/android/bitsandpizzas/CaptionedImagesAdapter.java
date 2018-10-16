010c5aa963641be9a8f05e3db98fb16ad887c9ec
Eric Emery <eric.r.emery@gmail.com>
2018-10-16 17:01:25 -0500

    feat: Changed the PizzaFragment to use RecyclerView instead of ListView

---
+++ app/src/main/java/com/eemery/android/bitsandpizzas/CaptionedImagesAdapter.java
@@ -0,0 +1,58 @@
+package com.eemery.android.bitsandpizzas;
+
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
+
+    private String[] captions;
+    private int[] imageIds;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // Define the view to be used for each data item
+        private CardView cardView;
+
+        public ViewHolder(CardView v) {
+            super(v);
+            cardView = v;
+        }
+    }
+
+    public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
+        this.captions = captions;
+        this.imageIds = imageIds;
+    }
+
+    @Override
+    public int getItemCount() {
+        return captions.length;
+    }
+
+    @NonNull
+    @Override
+    public CaptionedImagesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        // Code to instantiate the ViewHolder
+        CardView cv = (CardView) LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.card_captioned_image, viewGroup, false);
+        return new ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        CardView cardView = viewHolder.cardView;
+        ImageView imageView = cardView.findViewById(R.id.info_image);
+        Drawable drawable = ContextCompat.getDrawable(cardView.getContext(), imageIds[i]);
+        imageView.setImageDrawable(drawable);
+        imageView.setContentDescription(captions[i]);
+        TextView textView = cardView.findViewById(R.id.info_text);
+        textView.setText(captions[i]);
+    }
+}
---
+++ app/src/main/java/com/eemery/android/bitsandpizzas/Pizza.java
@@ -0,0 +1,25 @@
+package com.eemery.android.bitsandpizzas;
+
+public class Pizza {
+
+    private String name;
+    private int imageResourceID;
+
+    public static final Pizza[] pizzas = {
+            new Pizza("Diavolo", R.drawable.diavolo),
+            new Pizza("Funghi", R.drawable.funghi)
+    };
+
+    public Pizza(String name, int imageResourceID) {
+        this.name = name;
+        this.imageResourceID = imageResourceID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+}
